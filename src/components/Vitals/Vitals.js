a16078db8d33f75ff68ceedd394c06abb64638d0
Emina Basic <ebasic3@etf.unsa.ba>
2023-08-15 15:16:19 +0200

    vitals list, search, edit and create

--- src/components/Vitals/Vitals.js
+++ src/components/Vitals/Vitals.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import {
-  Container, Icon, Segment, Card, Divider, Button,
+  Container, Icon, Segment, Card, Divider, Button, Input,
 } from "semantic-ui-react";
 import styled from "styled-components";
+import debounce from "lodash.debounce";
+import { toast } from "react-toastify";
 
-import { getVitals } from "api/vitals";
-import Loader from "components/Loader";
+import { editVital, getVitals, postVital } from "api/vitals";
+import VitalsModalCreate from "./VitalsModalCreate";
+import VitalsModalEdit from "./VitalsModalEdit";
 
 const StyledContainer = styled(Container)`
   && {
     display: flex;
+    margin: 1rem;
     flex-direction: column;
     justify-content: center;
     padding-bottom: 2rem;
@@ -29,22 +33,101 @@ const StyledHeader = styled.h1`
   margin:0
 `;
 
+const StyledCardGroup = styled(Card.Group)`
+  &&& {
+    margin-top: 1rem;
+  }
+`;
+
 const SmallerCard = styled(Card)`
   &&& {
     width: 15rem;
   }
 `;
 
+const StyledCardHeader = styled(Card.Header)`
+  &&&&& {
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+  }
+`;
+
 const Vitals = () => {
   const [error, setError] = useState();
   const [loading, setLoading] = useState();
-  const [vitals, setVitals] = useState();
+  const [vitals, setVitals] = useState([]);
+  const [selectedVital, setSelectedVital] = useState([]);
+
+  const [showCreate, setShowCreate] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+
+  const debouncedSearch = debounce(async (query) => {
+    try {
+      setLoading(true);
+      const filters = {
+        search: query,
+      };
+      const results = await getVitals(filters);
+      setVitals(results);
+      setError("");
+    } catch (e) {
+      setError("Unable to fetch vitals");
+    } finally {
+      setLoading(false);
+    }
+  }, 300);
+
+  const handleClick = () => {
+    setShowCreate(!showCreate);
+  };
+
+  const handleCreate = async (values) => {
+    try {
+      await postVital(values);
+      toast.success("Vital added!");
+      debouncedSearch("");
+    } catch (err) {
+      toast.error("Unable to add vital!");
+    } finally {
+      setShowCreate(!showCreate);
+    }
+  };
+
+  const handleClickEdit = () => {
+    setShowEdit(!showEdit);
+  };
+
+  const handleEdit = async (values) => {
+    try {
+      await editVital(values.id, values);
+      toast.success("Vital updated!");
+      debouncedSearch("");
+    } catch (err) {
+      toast.error("Unable to update vital!");
+    } finally {
+      handleClickEdit(!showEdit);
+    }
+  };
+
+  const handleSearch = (value) => {
+    debouncedSearch(value);
+  };
+
+  const handleVitalClick = (v) => {
+    setSelectedVital(v);
+    handleClickEdit();
+  };
 
   useEffect(() => {
     const fetch = async () => {
       try {
         setLoading(true);
-        const response = await getVitals();
+        const filters = {
+          search: "",
+        };
+        const response = await getVitals(filters);
         setVitals(response);
       } catch (e) {
         setError("Unable to fetch vitals");
@@ -69,37 +152,69 @@ const Vitals = () => {
 
   return (
     <StyledContainer>
+      <VitalsModalCreate
+        show={showCreate}
+        handleClick={handleClick}
+        handleCreate={handleCreate}
+      />
+      <VitalsModalEdit
+        show={showEdit}
+        selectedVital={selectedVital}
+        handleClick={handleClickEdit}
+        handleEdit={handleEdit}
+      />
       <StyledTopContainer>
         <StyledHeader>Vitals</StyledHeader>
-        <Button size="small">
+        <Button size="small" onClick={handleClick}>
           <Icon name="plus square outline" />
           Add Vital
         </Button>
       </StyledTopContainer>
       <Divider />
-      <Loader isActive={loading} inverted />
-      {vitals && (
-        <Card.Group>
-          {vitals?.map((v) => (
-            <SmallerCard
-              color="teal"
-              header={v.name}
-              description={`Unit measurement: ${v.unitMeasurement}`}
-              extra={(
-                <>
-                  Lower limit:
-                  {" "}
-                  {v.lowerLimit}
-                  <br />
-                  Upper limit:
-                  {" "}
-                  {v.upperLimit}
-                </>
-            )}
-            />
-          ))}
-        </Card.Group>
-      )}
+      <Input
+        fluid
+        selection
+        search
+        placeholder="Search..."
+        onChange={(e, { value }) => {
+          handleSearch(value);
+        }}
+        loading={loading}
+      />
+      {vitals === null || vitals?.length === 0
+        ? (
+          <StyledContainer>
+            <Segment inverted color="blue" tertiary>
+              <Icon name="x" />
+              Vitals not found!
+            </Segment>
+          </StyledContainer>
+        )
+        : (
+          <StyledCardGroup>
+            {vitals?.map((v) => (
+              <SmallerCard color="teal" onClick={() => handleVitalClick(v)}>
+                <Card.Content>
+                  <StyledCardHeader>
+                    {v.name}
+                  </StyledCardHeader>
+                  <Card.Description>{`Unit measurement: ${v.unitMeasurement}`}</Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                  <>
+                    Lower limit:
+                    {" "}
+                    {v.lowerLimit}
+                    <br />
+                    Upper limit:
+                    {" "}
+                    {v.upperLimit}
+                  </>
+                </Card.Content>
+              </SmallerCard>
+            ))}
+          </StyledCardGroup>
+        )}
     </StyledContainer>
   );
 };
---
+++ src/components/Vitals/VitalsModalEdit.js
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import {
+  Button, Modal,
+} from "semantic-ui-react";
+import styled from "styled-components";
+import * as Yup from "yup";
+import { Form } from "formik-semantic-ui-react";
+import { Formik } from "formik";
+
+import InputField from "components/InputField";
+
+const StyledTopContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: row;
+  gap: 5rem;
+`;
+
+const TopInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  gap: 0.5rem;
+`;
+
+const ButtonGroup = styled.div`
+  padding-top: 2rem;
+  padding-bottom: 1.5rem;
+  float: right;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: row;
+  gap: 2rem;
+  width: 100%;
+`;
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .typeError("Name value must be a string")
+    .max(15, "Must be 15 characters or less")
+    .required("Please enter name"),
+  unitMeasurement: Yup.string()
+    .required("Please enter unit measurement"),
+  lowerLimit: Yup.number().typeError("Lower limit must be a number").required("Please select lower limit"),
+  upperLimit: Yup.number().typeError("Upper limit must be a number").required("Please select upper limit"),
+});
+
+const VitalsModalEdit = ({
+  selectedVital, show, handleClick, handleEdit,
+}) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (values) => {
+    setLoading(true);
+    handleEdit(values);
+    setLoading(false);
+  };
+
+  return (
+    <Modal
+      open={show}
+      onClose={handleClick}
+    >
+      <Modal.Header>Add Vital</Modal.Header>
+      <Modal.Content>
+        {selectedVital
+        && (
+        <Formik
+          initialValues={selectedVital}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {() => (
+            <Form>
+              <StyledTopContainer>
+                <TopInfo>
+                  <InputField
+                    label="Name"
+                    name="name"
+                    type="text"
+                    placeholder="Weight"
+                  />
+                  <InputField
+                    label="Unit Measurement"
+                    name="unitMeasurement"
+                    type="text"
+                    placeholder="kg"
+                  />
+                  <FlexContainer>
+                    <InputField
+                      label="Lower Limit"
+                      name="lowerLimit"
+                      type="text"
+                      placeholder="0"
+                    />
+                    <InputField
+                      label="Upper Limit"
+                      name="upperLimit"
+                      type="text"
+                      placeholder="700"
+                    />
+                  </FlexContainer>
+
+                </TopInfo>
+              </StyledTopContainer>
+              <Modal.Actions>
+                <ButtonGroup>
+                  <Button color="grey" onClick={handleClick} disabled={loading}>
+                    Cancel
+                  </Button>
+                  <Button
+                    type="submit"
+                    content="Update"
+                    positive
+                    disabled={loading}
+                    loading={loading}
+                  />
+                </ButtonGroup>
+              </Modal.Actions>
+            </Form>
+          )}
+        </Formik>
+        )}
+      </Modal.Content>
+
+    </Modal>
+  );
+};
+
+export default VitalsModalEdit;
